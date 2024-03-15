9d9d94912ec79d27754195b093e62cd94d481a82
Marilisa Moio <moiomarilisa@gmail.com>
2024-03-15 16:04:04 +0100

    chore: add static version of basic functions

--- js/script.js
+++ js/script.js
@@ -7,3 +7,44 @@
 //- con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
 //- con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 //- con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+
+//quando l'utente clicca il bottone
+    //recupera le scelte dell'utente e genera i dati che ti servono sulla base di essa
+    // genera n div da 1 a 100-81-49
+    // popolali con il numero corrispondente
+    //aggiungi le classi
+    //aggiungi il secondo clickevent
+        //aggiungi la classe activated
+        //logga il numero cliccato
+
+const squareContainer = document.querySelector(".container");
+let number = 30;
+let choice = "easy"
+
+for (let i = 1; i <= number; i++){
+    const newSquare = createDiv(i, choice);
+    squareContainer.append(newSquare);
+}
+
+// FUNCTIONS
+//funzione per generare un div, contentente un numero e una serie di classi, al quale poi viene agganciato un event listener che al click gli altribuisce una classe aggiuntiva e stampa in console il numero contenuto
+//argomenti:
+    //number, int -> per assegnare il numero all'interno del div
+    //choice, str -> per assegnare una classe
+// return: elemento del DOM
+function createDiv(number, choice){
+    const newDiv = document.createElement("div");
+
+    newDiv.className = `square ${choice}`;
+    newDiv.innerHTML = `<span>${number}</span>`;
+
+    newDiv.addEventListener("click", function(){
+        this.classList.toggle("activated");
+
+        if (this.classList.contains("activated")){
+            console.log(this.children[0].innerHTML)
+        }
+    })
+
+    return newDiv
+}
